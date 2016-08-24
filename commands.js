810857bf7c80f55cf6f207f65c081cf4a050ae66
zyresus <zyresus@users.noreply.github.com>
2016-08-24 16:55:49 +0100

    Update commands.js

--- commands.js
+++ commands.js
@@ -323,6 +323,7 @@ exports.commands = {
 			yellow: '#yellow',
 			zinnia: '#zinnia',
 			clemont: '#clemont',
+			showtan: '#showtan',
 		};
 		if (avatarid in avatarTable) {
 			avatar = avatarTable[avatarid];
